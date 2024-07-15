eda3e28e615876d3a36713c7d3fac8d163f9e766
AdityaU101 <111223966+AdityaU101@users.noreply.github.com>
2024-07-15 05:40:31 +0530

    containerized1

--- cons/src/App.jsx
+++ cons/src/App.jsx
@@ -4,12 +4,10 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Register from "./components/register";
 import Home from "./components/Home";
 import Contact from "./components/contact";
-import "./App.css";
+import "/Styles/App.css";
 import Login from "./components/login";
 
 const App = () => {
-  
-
   return (
     <div>
       <BrowserRouter>
